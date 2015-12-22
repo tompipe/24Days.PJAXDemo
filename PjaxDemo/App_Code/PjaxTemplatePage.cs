b8651dc4454168effb72353e3079659ef543e4eb
Tom Pipe <tompipe2008@gmail.com>
2015-12-22 00:47:41 +0000

    Break pjax container content out of site wrappers

---
+++ PjaxDemo/App_Code/PjaxTemplatePage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Web.Mvc;
+
+namespace PjaxDemo.App_Code
+{
+    public abstract class PjaxTemplatePage : UmbracoTemplatePage
+    {
+        public bool IsPjaxRequest
+        {
+            get
+            {
+                return Request.Headers["X-Requested-With"] == "XMLHttpRequest" && Request.QueryString["pjax"] == "true";
+            }
+        }
+
+        public string TrySwitchToPjaxLayout(string fallback)
+        {
+            return IsPjaxRequest ? "~/Views/Shared/_PjaxLayout.cshtml" : fallback;
+        }
+    }
+}
